48507169153715d086c10c1e2ce86896ad4664d9
nicholsa1954 <67351799+nicholsa1954@users.noreply.github.com>
2023-08-10 08:48:18 -0500

    adding files from "MappingWisconsinPoliticalTerrain" project

---
+++ plotlytools.py
@@ -0,0 +1,81 @@
+ctv_map = {'C':'City of', 'V':'Village of', 'T':'Town of'}
+
+import plotly.graph_objs as go
+
+def GetChoroplethMapbox(gdf, gjsn, variable, range, colorscale, 
+                        marker_line_color, marker_line_width, marker_opacity,
+                        hoverinfo, show_scale):
+    return go.Choroplethmapbox(geojson = gjsn,
+        locations = gdf['id'],
+        z = gdf[variable],
+        featureidkey = 'properties.id',
+        name = '',
+        colorscale = colorscale, 
+        zmin = range[0], zmax = range[1],
+        marker_line_color = marker_line_color, 
+        marker_line_width = marker_line_width, 
+        marker_opacity = marker_opacity,
+        hoverinfo = hoverinfo,
+        showscale = show_scale, visible = True)
+
+def GetScattermapbox(df, size, color, name, showlegend = True):
+    return go.Scattermapbox(
+        lat=df.loc[:, 'lat'],
+        lon=df.loc[:, 'lng'],
+        showlegend = showlegend,
+        name=name,
+        mode = 'markers',
+        marker=go.scattermapbox.Marker(
+            symbol='circle',
+            size=size,
+            color=color,
+            opacity=1.0,
+            allowoverlap=True))
+
+def GetOutlinemapbox(gdf, gjsn, colorscale, line_color, line_width):
+    return go.Choroplethmapbox(geojson = gjsn, 
+        locations = gdf['id'], 
+        z = gdf['z_layer'], 
+        name = '',
+        hoverinfo = 'skip',
+        featureidkey = 'properties.id',
+        colorscale = colorscale,
+        marker_line_color = line_color, 
+        marker_line_width = line_width, 
+        marker_opacity = 1.0,
+        showscale = False, visible = True)
+
+def CreateHoverTemplate(gdf):
+    result = []
+    for row in gdf.itertuples():
+        pct = f"""{100*row.LatinxVAPPct:.0f}%"""
+        row1 = ' '.join(['<b>'+ctv_map[row.CTV], row.MCD_NAME, 'Ward', row.WARDID+'</b>'])
+        row2 = ' '.join(['Latinx VAP:', str(row.LatinxVAP)])
+        row3 = ' '.join(['Total VAP:', str(row.VAP)])
+        row4 = ' '.join(['Latinx VAP / Total VAP:', pct])
+        template = '<br>'.join([row1, row2, row3, row4])
+        result.append(template)
+    return result
+
+def GetHoverlabelMapbox(gdf, template, color):
+    return go.Scattermapbox(
+        mode = 'text',
+        name = '',
+        lat = gdf.loc[:, 'lat'],
+        lon = gdf.loc[:, 'lon'],
+        hovertemplate = template,
+        hoverlabel=dict(
+            bgcolor=color))
+
+def GetStaticLabelMapbox(gdf, color, size):
+    return go.Scattermapbox(
+    mode='text',
+    name = '',
+    hoverinfo = 'skip',
+    lat=gdf.loc[:, 'lat'],
+    lon=gdf.loc[:, 'lon'],
+    text = gdf.loc[:, 'id'],
+    opacity=1.0,
+    textfont={"color":color,"size":size},
+    texttemplate = '%{text}',
+    textposition='middle center')
---
+++ vdlfcommon.py
@@ -0,0 +1,16 @@
+vdlf_target_counties = [
+    'Brown', 'Dane', 'Fond du Lac','Kenosha', 'Manitowoc', 
+    'Milwaukee', 'Outagamie', 'Racine', 'Rock', 
+    'Sheboygan', 'Trempealeau', 'Walworth', 'Waukesha', 'Winnebago']
+
+vdlf_target_cities = [
+    'Green Bay', 'Madison', 'Fond du Lac', 'Kenosha', 'Manitowoc',
+    'Milwaukee', 'Appleton', 'Racine', 'Beloit',
+    'Sheboygan', 'Arcadia', 'Delevan', 'Waukesha', 'Oshkosh']
+
+vdlf_target_fips = ['55009', '55025', '55039', '55059', '55073', 
+                    '55079', '55087', '55101', '55105', 
+                    '55117', '55121', '55127', '55133', '55139']
+
+county_fips_dict = dict(zip(vdlf_target_counties, vdlf_target_fips))
+county_city_dict = dict(zip(vdlf_target_counties, vdlf_target_cities))
