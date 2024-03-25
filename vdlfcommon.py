890d9989291ba80f71c6f67e8299026e84e51d58
Alan Nichols <nichols.alan@gmail.com>
2024-03-25 10:09:27 -0500

    adding Calumet County

--- vdlfcommon.py
+++ vdlfcommon.py
@@ -1,14 +1,14 @@
 vdlf_target_counties = [
-    'Brown', 'Dane', 'Fond du Lac','Kenosha', 'Manitowoc', 
+    'Brown', 'Calumet', 'Dane', 'Fond du Lac','Kenosha', 'Manitowoc', 
     'Milwaukee', 'Outagamie', 'Racine', 'Rock', 
     'Sheboygan', 'Trempealeau', 'Walworth', 'Waukesha', 'Winnebago']
 
 vdlf_target_cities = [
-    'Green Bay', 'Madison', 'Fond du Lac', 'Kenosha', 'Manitowoc',
+    'Green Bay', 'Appleton', 'Madison', 'Fond du Lac', 'Kenosha', 'Manitowoc',
     'Milwaukee', 'Appleton', 'Racine', 'Beloit',
     'Sheboygan', 'Arcadia', 'Delevan', 'Waukesha', 'Oshkosh']
 
-vdlf_target_fips = ['55009', '55025', '55039', '55059', '55073', 
+vdlf_target_fips = ['55009', '55015', '55025', '55039', '55059', '55073', 
                     '55079', '55087', '55101', '55105', 
                     '55117', '55121', '55127', '55133', '55139']
 
