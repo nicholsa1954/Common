aa5f728c75d69b3bbb671af5d6bbc73c5721c24a
Alan Nichols <nichols.alan@gmail.com>
2025-10-15 12:04:03 -0500

    Handle invalid email

--- emailtools.py
+++ emailtools.py
@@ -2,7 +2,11 @@ import re
 
 ### Source: https://medium.com/@python-javascript-php-html-css/the-best-regular-expression-for-email-address-verification-42bf83ba2885
 def ValidateEmail(email):
+    if isinstance(email, float) or email == "" or email != email: return "" # in case email is NaN
     # Regular expression for email validation
     regex  = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     # Return true if email matches regex, false otherwise
-    return re.match(regex, email) is not None
+    
+    if re.match(regex, email) is not None: return email
+    else:
+        return (f'Invalid email: {email}')
