54a9da1c210f759691fdf42314066e4d1709f64b
Alan Nichols <nichols.alan@gmail.com>
2026-08-01 10:40:53 -0500

    Update to use the new async version of the NextCloud API

--- nextcloudtools.py
+++ nextcloudtools.py
@@ -1,6 +1,7 @@
 import io
+import asyncio
 import pandas as pd
-from nc_py_api import Nextcloud
+from nc_py_api import AsyncNextcloud
 
 import sys
 sys.path.append('./')
@@ -22,13 +23,13 @@ http = urllib3.PoolManager(
     ca_certs=certifi.where()
 )
 
-def InitializeNextCloudDataFrame(path, data_file, remote_file = False):
+async def InitializeNextCloudDataFrame(path, data_file, remote_file = False):
     
     if remote_file and not testVPNConnection():
         print('No VPN connection -- returning empty DataFrame.')
         return pd.DataFrame()
     
-    nxc = Nextcloud(
+    nxc = AsyncNextcloud(
         nextcloud_url = NEXTCLOUD_URL,
         nc_auth_user = NEXTCLOUD_USERNAME,
         nc_auth_pass = NEXTCLOUD_PASSWORD,
@@ -36,18 +37,18 @@ def InitializeNextCloudDataFrame(path, data_file, remote_file = False):
             'verify': True  # verify ssl
             }) 
     
-    data_file = nxc.files.download(''.join([path, data_file]))
+    data_file = await nxc.files.download(''.join([path, data_file]))
     data_bytes_io = io.BytesIO(data_file)
     data_bytes_io.seek(0)
     return pd.read_excel(data_bytes_io, engine='openpyxl')
 
-def InitializeNextCloudDataFramesDict(remote_path, data_file, remote_file = False):
+async def InitializeNextCloudDataFramesDict(remote_path, data_file, remote_file = False):
     
     if remote_file and not testVPNConnection():
         print('No VPN connection -- returning empty DataFrame.')
         return pd.DataFrame()
     
-    nxc = Nextcloud(
+    nxc = AsyncNextcloud(
         nextcloud_url = NEXTCLOUD_URL,
         nc_auth_user = NEXTCLOUD_USERNAME,
         nc_auth_pass = NEXTCLOUD_PASSWORD,
@@ -56,12 +57,12 @@ def InitializeNextCloudDataFramesDict(remote_path, data_file, remote_file = Fals
             }) 
     
     
-    data_file = nxc.files.download(''.join([remote_path, data_file]))
+    data_file = await nxc.files.download(''.join([remote_path, data_file]))
     data_bytes_io = io.BytesIO(data_file)
     data_bytes_io.seek(0)
     return pd.read_excel(data_bytes_io, sheet_name=None, engine='openpyxl')
 
-def UploadDfListToNextCloud(remote_filename, df_list, sheet_names, overwrite = True):
+async def UploadDfListToNextCloud(remote_filename, df_list, sheet_names, overwrite = True):
 
     buf = io.BytesIO()
     with pd.ExcelWriter(buf,  engine='openpyxl') as writer:
@@ -81,7 +82,7 @@ def UploadDfListToNextCloud(remote_filename, df_list, sheet_names, overwrite = T
         nc_auth_pass = NEXTCLOUD_PASSWORD)
 
     buf.seek(0)
-    result = nxc.files.upload_stream(
+    result = await nxc.files.upload_stream(
         path = remote_filename,
         fp = buf,
         kwargs={'overwrite': overwrite, 
@@ -90,13 +91,13 @@ def UploadDfListToNextCloud(remote_filename, df_list, sheet_names, overwrite = T
     return result
 
 
-def WriteMailingListToNextCloud(remote_filename, email_list, overwrite = True):
+async def WriteMailingListToNextCloud(remote_filename, email_list, overwrite = True):
     buf = io.BytesIO()
     for email in sorted(email_list):
         buf.write(''.join([email,'\n']).encode('utf-8'))
     buf.seek(0)
     
-    nxc = Nextcloud(
+    nxc = AsyncNextcloud(
         nextcloud_url = NEXTCLOUD_URL,
         nc_auth_user = NEXTCLOUD_USERNAME,
         nc_auth_pass = NEXTCLOUD_PASSWORD,
@@ -104,7 +105,7 @@ def WriteMailingListToNextCloud(remote_filename, email_list, overwrite = True):
             'verify': True  # verify ssl
             })     
 
-    result = nxc.files.upload_stream(
+    result = await nxc.files.upload_stream(
         path = remote_filename,
         fp = buf,
         kwargs={'overwrite': overwrite, 
@@ -113,14 +114,14 @@ def WriteMailingListToNextCloud(remote_filename, email_list, overwrite = True):
     return result 
 
 
-def ReadFileListFromNextCloud(subdir = 'Shared/'):
-    nxc = Nextcloud(
+async def ReadFileListFromNextCloud(subdir = 'Shared/'):
+    nxc = AsyncNextcloud(
     nextcloud_url = NEXTCLOUD_URL,
     nc_auth_user = NEXTCLOUD_USERNAME,
     nc_auth_pass = NEXTCLOUD_PASSWORD)
     
     file_list = []
-    all_files = [i for i in nxc.files.listdir(depth=-1) if not i.is_dir and i.user_path.startswith(subdir)]
+    all_files = [i for i in await nxc.files.listdir(depth=-1) if not i.is_dir and i.user_path.startswith(subdir)]
     for obj in all_files:
         file_list.append(obj.user_path)
         
